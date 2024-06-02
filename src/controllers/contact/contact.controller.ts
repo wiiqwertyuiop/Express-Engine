9650294b53c5acb13cc03a9a268e5cf16e42881f
bob sagat <wiiqwertyuiop@gmail.com>
2024-06-02 12:09:59 -0400

    touchups

--- src/controllers/contact/contact.controller.ts
+++ src/controllers/contact/contact.controller.ts
@@ -1,24 +1,21 @@
 import { Request, Response } from "express";
 import { Controller, Get, Put } from "../../common/decorators/controller.decorator";
 import { compileTemplate } from "../../common/template/template";
-import { Contact } from "../../models/contact/contact.model";
+import { getContact } from "../../models/contact/contact.model";
 
 @Controller('/contact', { needsReferer: true })
 export class ContactController {
 
   @Get()
   getContact(request: Request, response: Response) {
-    if (!request.cookies['contact']) {
-      const placeholder = new Contact("Joe", "Blow");
-      response.cookie('contact', placeholder, { httpOnly: true, sameSite: 'strict', secure: true })
-      return response.send(compileTemplate("contact/form.html", placeholder));
-    }
-    return response.send(compileTemplate("contact/form.html", request.cookies['contact']));
+    const contact = getContact(request);
+    return response.send(compileTemplate("contact/form.html", contact));
   }
 
   @Get('/edit')
   getContactEdit(request: Request, response: Response) {
-    return response.send(compileTemplate("contact/editForm.html", request.cookies['contact']));
+    const contact = getContact(request);
+    return response.send(compileTemplate("contact/editForm.html", contact));
   }
 
   @Put()
--- src/models/contact/contact.model.ts
+++ src/models/contact/contact.model.ts
@@ -1,9 +1,16 @@
-export class Contact {
+import { Request } from "express";
+
+class Contact {
     private email: string;
     constructor(private firstName: string, private lastName: string) {
         this.email = `${firstName}@${lastName}.com`.toLowerCase();
     }
-    getContact() {
-        return { firstName: this.firstName, lastName: this.lastName, email: this.email };
+}
+
+export function getContact(request: Request): Contact {
+    const savedContact = request.cookies['contact'];
+    if (!savedContact) {
+        return new Contact("Joe", "Blow");
     }
+    return savedContact;
 }
